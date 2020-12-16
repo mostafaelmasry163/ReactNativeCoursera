6f605705787f9e9681a38af59b0d3756a33171ef
mostafaelmasry163 <mostelmasry93@gmail.com>
2020-12-16 06:18:33 +0200

    persist redux store

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -22,7 +22,7 @@ const mapDispatchToProps = dispatch => ({
 function RenderDish(props) {
     const dish = props.dish;
 
-    handleViewRef = ref => this.view =ref;
+    handleViewRef = ref => this.view = ref;
 
     const recognizeDrag = ({ moveX, moveY, dx, dy}) => {
         if (dx < -200 )
